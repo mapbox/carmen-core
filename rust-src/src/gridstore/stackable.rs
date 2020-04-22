e05ffb9842e27d7a859fbde6db3c2dd5c144272e
Andrew Pendleton <andrew@mapbox.com>
2020-04-22 03:35:48 -0400

    First-pass constrained stackable using generational arena storage

--- rust-src/src/gridstore/stackable.rs
+++ rust-src/src/gridstore/stackable.rs
@@ -1,15 +1,18 @@
 #![allow(dead_code)]
 use std::borrow::Borrow;
-use std::collections::{BTreeMap, HashSet};
+use std::collections::{BTreeMap, HashMap, HashSet};
 use std::fmt::Debug;
 
 use crate::gridstore::common::*;
 use crate::gridstore::store::*;
 
+use generational_arena::{Arena, Index as ArenaIndex};
+use ordered_float::OrderedFloat;
+
 #[derive(Debug, Clone)]
 pub struct StackableNode<'a, T: Borrow<GridStore> + Clone + Debug> {
     pub phrasematch: Option<&'a PhrasematchSubquery<T>>,
-    pub children: Vec<StackableNode<'a, T>>,
+    pub children: Vec<ArenaIndex>,
     pub bmask: HashSet<u16>,
     pub mask: u32,
     pub idx: u16,
@@ -24,25 +27,117 @@ impl<'a, T: Borrow<GridStore> + Clone + Debug> StackableNode<'a, T> {
 }
 
 //tree traversal used only for tests
-pub fn bfs<T: Borrow<GridStore> + Clone + Debug>(root: StackableNode<T>) -> Vec<StackableNode<T>> {
+pub fn bfs<T: Borrow<GridStore> + Clone + Debug>(tree: StackableTree<T>) -> Vec<StackableNode<T>> {
     let mut node_vec: Vec<StackableNode<T>> = vec![];
     let mut stack: Vec<_> = vec![];
 
-    stack.push(root);
+    stack.push(tree.root.clone());
 
     while stack.len() > 0 {
         let node = stack.pop().unwrap();
         node_vec.push(node.clone());
-        for child in node.children {
-            stack.push(child);
+        for maybe_child in node.children {
+            if let Some(child) = tree.arena.get(maybe_child) {
+                stack.push(child.clone());
+            }
         }
     }
     return node_vec;
 }
 
+pub const LEAF_SOFT_MAX: usize = 1024;
+
+#[derive(Debug, Clone)]
+pub struct ArenaManager<'a, T: Borrow<GridStore> + Clone + Debug> {
+    arena: Arena<StackableNode<'a, T>>,
+    // this is a map from max_relev to the count of leaves with that max_relev, and a list of all
+    // the arena indexes (leaf or otherwise) with that max_index
+    relev_map: HashMap<OrderedFloat<f64>, (usize, Vec<ArenaIndex>)>,
+    min_relev: OrderedFloat<f64>,
+    total_leaves: usize,
+    soft_max: usize
+}
+
+impl<'a, T: Borrow<GridStore> + Clone + Debug> ArenaManager<'a, T> {
+    fn new() -> Self {
+        ArenaManager {
+            arena: Arena::new(),
+            relev_map: HashMap::new(),
+            min_relev: OrderedFloat(std::f64::MAX),
+            total_leaves: 0,
+            soft_max: LEAF_SOFT_MAX,
+        }
+    }
+
+    fn add(&mut self, node: StackableNode<'a, T>) -> Option<ArenaIndex> {
+        let max_relev = OrderedFloat(node.max_relev);
+        let is_leaf = node.children.len() == 0;
+
+        if self.total_leaves >= self.soft_max && max_relev < self.min_relev {
+            // we're constrained, and this is worse than our worst, so don't keep it
+            None
+        } else {
+            // we're definitely going to add this
+            let arena_index = self.arena.insert(node);
+
+            let mut relev_entry = self.relev_map.entry(max_relev).or_insert((0, Vec::new()));
+
+            if is_leaf { relev_entry.0 += 1; }
+            relev_entry.1.push(arena_index);
+            self.total_leaves += 1;
+
+            if self.total_leaves < self.soft_max {
+                // this was an unconstrained add, so just update min if necessary and move on
+                if max_relev < self.min_relev {
+                    self.min_relev = max_relev;
+                }
+            } else {
+                // this was a constrained add. if we inserted into the min bucket, we're done,
+                // but if we added to some better bin than that, we may be able to cull the min
+                // bucket and choose a new min
+                if max_relev > self.min_relev {
+                    // if we're inserting into a bin other than the worst one, we might be able
+                    // to cull the worst one
+                    let min_count = self.relev_map.get(&self.min_relev).expect("must contain min_relev").0;
+                    let total_without_min = self.total_leaves - min_count;
+                    if total_without_min >= self.soft_max {
+                        // we can make due without the minimum bin
+                        self.cull_min();
+                    }
+                }
+            }
+            Some(arena_index)
+        }
+    }
+
+    fn cull_min(&mut self) {
+        if let Some((min_leaf_count, min_nodes)) = self.relev_map.remove(&self.min_relev) {
+            self.total_leaves -= min_leaf_count;
+            for node_index in min_nodes {
+                self.arena.remove(node_index);
+            }
+        }
+        // pick a new min
+        self.min_relev = self.relev_map.keys().min().map(|min| *min).unwrap_or(OrderedFloat(std::f64::MAX));
+    }
+
+    #[inline(always)]
+    pub fn get(&self, index: ArenaIndex) -> Option<&StackableNode<'a, T>> {
+        self.arena.get(index)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct StackableTree<'a, T: Borrow<GridStore> + Clone + Debug> {
+    pub root: StackableNode<'a, T>,
+    pub arena: ArenaManager<'a, T>
+}
+
 pub fn stackable<'a, T: Borrow<GridStore> + Clone + Debug>(
     phrasematches: &'a Vec<PhrasematchSubquery<T>>,
-) -> StackableNode<'a, T> {
+) -> StackableTree<'a, T> {
+    let mut arena: ArenaManager<'a, T> = ArenaManager::new();
+
     let mut binned_phrasematches: BTreeMap<u16, Vec<&'a PhrasematchSubquery<T>>> = BTreeMap::new();
     for phrasematch in phrasematches {
         binned_phrasematches
@@ -51,55 +146,74 @@ pub fn stackable<'a, T: Borrow<GridStore> + Clone + Debug>(
             .push(phrasematch);
     }
     let binned_phrasematches: Vec<_> = binned_phrasematches.into_iter().map(|(_k, v)| v).collect();
-    binned_stackable(&binned_phrasematches, None, HashSet::new(), 0, 129, 0.0, 0, 0)
+    let mut counters = (0, 0);
+    let root = binned_stackable(&binned_phrasematches, None, HashSet::new(), 0, 129, 0.0, 0, 0, &mut arena, &mut counters);
+    println!("{:?} {}", counters, arena.arena.len());
+    StackableTree { root, arena }
 }
 
 fn binned_stackable<'b, 'a: 'b, T: Borrow<GridStore> + Clone + Debug>(
-    binned_phrasematch: &'b Vec<Vec<&'a PhrasematchSubquery<T>>>,
-    phrasematch_result: Option<&'a PhrasematchSubquery<T>>,
+    binned_phrasematches: &'b Vec<Vec<&'a PhrasematchSubquery<T>>>,
+    current_phrasematch: Option<&'a PhrasematchSubquery<T>>,
     bmask: HashSet<u16>,
     mask: u32,
     idx: u16,
-    max_relev: f64,
+    relev_so_far: f64,
     zoom: u16,
     start_type_idx: usize,
+    arena: &mut ArenaManager<'a, T>,
+    counters: &mut (usize, usize),
 ) -> StackableNode<'a, T> {
     let mut node = StackableNode {
-        phrasematch: phrasematch_result,
+        phrasematch: current_phrasematch,
         children: vec![],
         mask: mask,
         bmask: bmask,
         idx: idx,
-        max_relev: max_relev,
+        max_relev: relev_so_far,
         zoom: zoom,
     };
 
-    for (type_idx, phrasematch_group) in binned_phrasematch.iter().enumerate().skip(start_type_idx)
+    for (type_idx, phrasematch_group) in binned_phrasematches.iter().enumerate().skip(start_type_idx)
     {
-        for phrasematches in phrasematch_group.iter() {
-            if (node.mask & phrasematches.mask) == 0
-                && phrasematches.non_overlapping_indexes.contains(&node.idx) == false
+        for phrasematch in phrasematch_group.iter() {
+            if (node.mask & phrasematch.mask) == 0
+                && phrasematch.non_overlapping_indexes.contains(&node.idx) == false
             {
-                let target_mask = &phrasematches.mask | node.mask;
+                let target_mask = &phrasematch.mask | node.mask;
                 let mut target_bmask: HashSet<u16> = node.bmask.iter().cloned().collect();
                 let phrasematch_bmask: HashSet<u16> =
-                    phrasematches.non_overlapping_indexes.iter().cloned().collect();
+                    phrasematch.non_overlapping_indexes.iter().cloned().collect();
                 target_bmask.extend(&phrasematch_bmask);
-                let target_relev = 0.0 + phrasematches.weight;
+                let target_relev = relev_so_far + phrasematch.weight;
 
-                node.children.push(binned_stackable(
-                    &binned_phrasematch,
-                    Some(&phrasematches),
+                let child_node = binned_stackable(
+                    &binned_phrasematches,
+                    Some(&phrasematch),
                     target_bmask,
                     target_mask,
-                    phrasematches.idx,
+                    phrasematch.idx,
                     target_relev,
-                    phrasematches.store.borrow().zoom,
+                    phrasematch.store.borrow().zoom,
                     type_idx + 1,
-                ));
+                    arena,
+                    counters
+                );
+
+                let max_relev = child_node.max_relev;
+
+                if let Some(arena_index) = arena.add(child_node) {
+                    node.children.push(arena_index);
+
+                    if max_relev > node.max_relev {
+                        node.max_relev = max_relev;
+                    }
+                }
             }
         }
     }
+    counters.0 += 1;
+    if node.children.len() == 0 { counters.1 += 1; }
     node
 }
 /*
@@ -168,25 +282,22 @@ mod test {
         let phrasematch_results = vec![a1, b1, b2];
 
         let tree = stackable(&phrasematch_results);
-        let a1_children_ids: Vec<u32> = tree.clone().children[0]
-            .clone()
+        let a1_children_ids: Vec<u32> = tree.arena.get(tree.root.children[0]).unwrap()
             .children
             .iter()
-            .map(|node| node.phrasematch.as_ref().map(|p| p.match_keys[0].id).unwrap())
+            .map(|node_idx| tree.arena.get(*node_idx).unwrap().phrasematch.as_ref().map(|p| p.match_keys[0].id).unwrap())
             .collect();
         assert_eq!(vec![1, 2], a1_children_ids, "a1 can stack with b1 and b2");
-        let b1_children_ids: Vec<u32> = tree.clone().children[1]
-            .clone()
+        let b1_children_ids: Vec<u32> = tree.arena.get(tree.root.children[1]).unwrap()
             .children
             .iter()
-            .map(|node| node.phrasematch.as_ref().map(|p| p.match_keys[0].id).unwrap())
+            .map(|node_idx| tree.arena.get(*node_idx).unwrap().phrasematch.as_ref().map(|p| p.match_keys[0].id).unwrap())
             .collect();
         assert_eq!(0, b1_children_ids.len(), "b1 cannot stack with b2, same nmask");
-        let b2_children_ids: Vec<u32> = tree.clone().children[2]
-            .clone()
+        let b2_children_ids: Vec<u32> = tree.arena.get(tree.root.children[2]).unwrap()
             .children
             .iter()
-            .map(|node| node.phrasematch.as_ref().map(|p| p.match_keys[0].id).unwrap())
+            .map(|node_idx| tree.arena.get(*node_idx).unwrap().phrasematch.as_ref().map(|p| p.match_keys[0].id).unwrap())
             .collect();
         assert_eq!(0, b2_children_ids.len(), "b2 cannot stack with b1, same nmask");
     }
